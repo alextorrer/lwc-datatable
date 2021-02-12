aa2fd8a696eabd5c1bbeaca8235508f24b898fc8
alextorrer <alextorre0427@gmail.com>
2021-02-12 16:05:31 -0600

    Create a simple unit test and mock for the fetch function

---
+++ force-app/main/default/lwc/booksTable/__tests__/fetch.test.js
@@ -0,0 +1,18 @@
+import fetchBooks from '../fetchBooksHelper';
+
+global.fetch = jest.fn(() => {
+    return Promise.resolve({
+        json: () => Promise.resolve({ kind: 'books#volumes' })
+    });
+});
+
+beforeEach(() => {
+    fetch.mockClear();
+});
+
+describe('fetch-books-unit-test', () => {
+    it('contains items', async () => {
+        const json = await fetchBooks('salesforce');
+        await expect(json.kind).toBe('books#volumes');
+    });
+});
